0f21bcb9a8870720f93d8aae925c1a060895406b
aaronayitey <aaronayitey@gmail.com>
2024-11-10 23:55:51 +0000

    u

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -55,13 +55,9 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-]
-
-from django.urls import path
-from .views import add_book, edit_book, delete_book
-
-urlpatterns = [
     path('book/add/', add_book, name='add_book'),
     path('book/edit/<int:book_id>/', edit_book, name='edit_book'),
     path('book/delete/<int:book_id>/', delete_book, name='delete_book'),
+
 ]
+
