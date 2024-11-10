e8d01087adb6d92e48104aff8cbb1534174937d3
aaronayitey <aaronayitey@gmail.com>
2024-11-10 23:10:27 +0000

    Implement user authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,9 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView  # Import the views explicitly
+from .views import list_books, LibraryDetailView, CustomLoginView, CustomLogoutView, register  # Import the views explicitly
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 urlpatterns = [
@@ -9,4 +12,27 @@ urlpatterns = [
     
     # Class-based view for displaying details of a specific library
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
 ]
+
+
+# Login View (built-in)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout View (built-in)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration View (Custom)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
