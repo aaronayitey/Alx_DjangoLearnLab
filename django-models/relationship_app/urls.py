777cbb1846617ba083ada51bbac13d7955796151
aaronayitey <aaronayitey@gmail.com>
2024-11-10 22:42:36 +0000

    Update urlpatterns

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,8 +5,8 @@ from .views import list_books, LibraryDetailView  # Import the views explicitly
 
 urlpatterns = [
     # Function-based view for listing books
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
     
     # Class-based view for displaying details of a specific library
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
