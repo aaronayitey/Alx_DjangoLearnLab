cdca2b9f7e315a70715ab579008ead313e82875b
aaronayitey <aaronayitey@gmail.com>
2024-11-10 23:14:29 +0000

    Clean views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 
@@ -15,3 +18,23 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Login View (built-in)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout View (built-in)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration View (Custom)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in after registration
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
