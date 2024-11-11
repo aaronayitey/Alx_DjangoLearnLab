ed7b66c58f78c513336cadbf5abe430feedbdd1e
aaronayitey <aaronayitey@gmail.com>
2024-11-11 01:12:06 +0000

    ppp

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -57,7 +57,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('book/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 
 ]
 
